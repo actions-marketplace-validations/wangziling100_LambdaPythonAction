6b106a5b59249662e5352680a1b83e815be0f806
Bob Wang <wangziling1000@gmail.com>
2020-09-11 15:30:03 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,14 @@ if [ -f config.yaml ]; then
 else
     echo "app.py is not defined, a demo will be installed."
 fi
+if [ -f requirements.txt ];then
+    cp requirements.txt /workspace
+else
+    echo "requirements.txt ist not defined, a default one will be applied"
+fi
 old_dir=`pwd`
 cd /workspace
 bash script/fill_template.sh
 bash script/rename.sh
 make ci
+cd $old_dir
