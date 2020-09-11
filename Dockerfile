81f48599b051cccf19a380962ac09fe6ac3bf03d
Bob Wang <wangziling1000@gmail.com>
2020-09-11 14:24:45 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apk add --no-cache build-base curl bash &&\
     mkdir /github_workspace && \
     curl -fSL https://github.com/subchen/frep/releases/download/v1.3.10/frep-1.3.10-linux-amd64 -o /usr/local/bin/frep &&\
     chmod +x /usr/local/bin/frep && \
+    pip install aws-sam-cli && \
     cd /workspace
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -l
 
-a=$(ls /workspace)
-b=$(ls)
-echo "Hello $1"
-cp README.md /workspace
-echo "-----------"
-echo "all: $b"
-echo "action workspace: $a"
-echo "-----------"
-time=$(date)
-cp config.yaml /workspace
-cp app.py /workspace/demo
+echo "ci start..."
+echo "check configeration"
+if [ -f config.yaml ]; then
+    cp config.yaml /workspace
+else
+    echo "Config file doesn't exist"
+    exit -1
+fi
+if [ -f config.yaml ]; then
+    cp app.py /workspace/demo
+else
+    echo "app.py is not defined, a demo will be installed."
+fi
 old_dir=`pwd`
 cd /workspace
 bash script/fill_template.sh
 bash script/rename.sh
 make ci
-echo ::set-output name=time::$time
