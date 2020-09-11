ce499807f8dedde36a33a4763ca5a5c55a19b643
Bob Wang <wangziling1000@gmail.com>
2020-09-11 16:06:10 +0200

    test

--- demo/script/fill_template.sh
+++ demo/script/fill_template.sh
@@ -1,11 +1,11 @@
 old_dir=`pwd`
 cd $(dirname $0)
 if [[ "$BUILD_ENV" == 'ci' ]]; then
-    ./frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
+    ./frep ../template.yaml.template:../template.yml --load ../config.yaml --overwrite
     ./frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
     ./frep ../env.json.template:../env.json --load ../config.yaml --overwrite
 else
-    frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
+    frep ../template.yaml.template:../template.yml --load ../config.yaml --overwrite
     frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
     frep ../env.json.template:../env.json --load ../config.yaml --overwrite
 fi
