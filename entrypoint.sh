08e6a4d418e1358bf47ee7b69ca19b053b0843fd
Bob Wang <wangziling1000@gmail.com>
2020-09-11 12:00:42 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
 a=$(ls /github_workspace)
+b=$(ls)
 echo "Hello $1"
+echo "-----------"
+echo "all: $b"
 echo "github workspace: $a"
+echo "-----------"
 time=$(date)
 echo ::set-output name=time::$time
